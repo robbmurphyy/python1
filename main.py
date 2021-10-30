155023f5e43ee9f16308dfebd16817ef56f696f7
robbmurphyy <rmurphy272@g.rwu.edu>
2021-10-30 15:50:08 -0400

    original b4 any features

--- main.py
+++ main.py
@@ -1,27 +1,17 @@
 # Author: Robert Murphy
-# Date: 9/17/21
-# Purpose: Module 2 assignment
+# Date: 10/31/21
+# Purpose:  assignment 6
 
-def check_input_int(input):
-    try:
-        #try converting to int
-        num = int(input)
-        return 0
-    except ValueError:
-        try:
-            # see if its a float
-            num = float(input)
-            print("You put in a float, not a integer.")
-            return 1
-        except ValueError:
-            print("You put in a string. Put in an Integer please.")
-            return 1
+
 
 #checks to see if the input has all letters or - in it
 def check_string_input(input):
     word2 = input
     hyphen = "-"
-    TorF = word2.isalpha()
+    try:
+        TorF = word2.isalpha()
+    except:
+        print("You did not enter a string")
     if TorF == True:
         return 0
     if TorF == False and hyphen in word2:
@@ -31,30 +21,18 @@ def check_string_input(input):
         return 1
 
 
-def mod2():
-    # will take in a name as a string and then print that name back on console
-    user_name = input("Please enter your name: ")
-    print("You have a cool name " + user_name + ".")
-    # In my opinion names can have numbers ( elon musk's kid)
-    print("------------------")
-
-    # will ask the user for an int and them * it by itself and then print to console
-    mult_num = input("Please enter a integer number: ")
-    if check_input_int(mult_num) == 0:
-        print(mult_num, " * ", mult_num)
-        print("Result:", int(mult_num) * int(mult_num))
-        print("------------------")
+def pigLatin():
 
     # asks user for a word, counts num of letters, and then prints
     user_word = input("Please enter a word: ")
     if check_string_input(user_word) == 0:
-        length_of_word = len(user_word)
-        print("Your word is " + str(length_of_word) + " letters long.")
         print("------------------")
 
 
+
+
 if __name__ == '__main__':
-    mod2()
+    pigLatin()
 else:
     print("sorry couldn't run main")
 
