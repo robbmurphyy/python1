ee766765acbdc193aa167a4206b67d00766df236
robbmurphyy <rmurphy272@g.rwu.edu>
2021-10-30 16:34:31 -0400

    string mods1 are implemented

--- main.py
+++ main.py
@@ -17,22 +17,57 @@ def check_string_input(input):
     if TorF == False and hyphen in word2:
         return 0
     if TorF == False:
-        print("The string must have all letters. No numbers, symbols, or spaces.")
+        print("The string must have all letters. No numbers.")
         return 1
 
 
-def pigLatin():
+def str_mods(usr_choice):
 
-    # asks user for a word, counts num of letters, and then prints
-    user_word = input("Please enter a word: ")
+    # asks user for a phrase and then takes the first letter of each word and changes it to +5 of its ascii codepoint
+    user_word = input("Please enter a phrase: ")
     if check_string_input(user_word) == 0:
         print("------------------")
+
+    if usr_choice == 1:
+        new_str = list(user_word)
+
+        for x in user_word:
+            if x == 0:
+                first = chr(ord(user_word[0]) + 5)
+                new_str[0] = first
+            else:
+                if user_word[x] is " ":
+                    second = ord(user_word[x + 1])
+                    third = chr(second + 5)
+                    new_str[x + 1] = third
+
+        print("The new phrase is: ", new_str)
+
+    # will take whatever punctuation is in the phrase and turns it into a ?
+    if usr_choice == 2:
+        second_str = list(user_word)
+        punct_list = [".", "!", ",", ":", ";", "-", "_", "(", ")", "\"", "\'", "`"]
+
+        for y in user_word:
+            if user_word[y] in punct_list:
+                second_str[y] = "?"
+
+        print("The new phrase is: ", second_str)
 
 
 
 
 if __name__ == '__main__':
-    pigLatin()
+    choice = input("Enter 1 to change the first letter of each word. \nEnter 2 to change all punctuation to ?")
+    try:
+        int_choice = int(choice)
+    except:
+        print("Please enter either 1 or 2")
+
+    if (int_choice == 1 or int_choice == 2):
+        str_mods(int_choice)
+    else:
+        print("Could not run str_mods")
 else:
     print("sorry couldn't run main")
 
